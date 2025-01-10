af1c8f22ed010e1b625760c67ad04d148bcd441a
SushrithKbtech <155775482+SushrithKbtech@users.noreply.github.com>
2025-01-10 14:42:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 import Doctor from "./components/Doctor";
 import Patient from "./components/Patient";
 import VideoCall from "./components/VideoCall";
@@ -8,7 +9,8 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Doctor />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/doctor" element={<Doctor />} />
         <Route path="/patient" element={<Patient />} />
         <Route path="/call/:room" element={<VideoCall />} />
       </Routes>
